db441520364291f0f9abc61709e9454bd2c0189b
Farhan-Abid <shahabid628@gmail.com>
2022-05-07 22:19:13 +0600

    Delivered button updated

--- src/Pages/InventoryDetail/InventoryDetail.js
+++ src/Pages/InventoryDetail/InventoryDetail.js
@@ -3,33 +3,64 @@ import { Button } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 
 const InventoryDetail = () => {
-    
-    const {id} = useParams();
-    const [product,setProduct] = useState({});
-    
-    useEffect( () => {
-        const url =`http://localhost:5000/products/${id}`
+
+    const { id } = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/products/${id}`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setProduct(data));
+            .then(res => res.json())
+            .then(data => setProduct(data));
     }, []);
 
-    console.log(product);
-    
-    return (
-        <div>
-            <h2>Welcome to Inventory Detail: {product._id}</h2>
-            <h2>{product.name}</h2>
-            <h3>{product.description}</h3>
-            <h3>{product.image}</h3>
-            <h3>{product.price}</h3>
-            <h3>{product.quantity}</h3>
-            <h3>{product.supplier}</h3>
-            
-            
-    
-        </div>
-    );
-};
+    const handleUpdateProduct = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
 
-export default InventoryDetail;
+        const updatedProduct = { name, email };
+
+        // send data to the server
+        const url = `http://localhost:5000/products/${id}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedProduct),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                alert('users added succesfully!!!')
+                event.target.reset();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        }
+        console.log(product);
+
+        return (
+            <div>
+                <h2>Welcome to Inventory Detail: {product._id}</h2>
+                <h2>{product.name}</h2>
+                <h3>{product.description}</h3>
+                <h3>{product.image}</h3>
+                <h3>{product.price}</h3>
+                <h3>{product.quantity}</h3>
+                <h3>{product.supplier}</h3>
+                <form onSubmit={handleUpdateProduct}>
+                    <input type="number" name='number' placeholder='Quantity' required />
+                    <br />
+                    <input type="submit" value="Delivered" />
+                </form>
+
+
+            </div>
+        );
+    };
+
+    export default InventoryDetail;
---
+++ src/Pages/InventoryDetail/UpdateInventory/UpdateInventory.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const UpdateInventory = () => {
+    const handleUpdateProduct = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const updatedProduct = {name,email};
+
+        // send data to the server
+
+        fetch('http://localhost:5000/user', {
+  method: 'PUT', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(updatedProduct),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+  alert('users added succesfully!!!')
+  event.target.reset();
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleUpdateProduct}>
+                <input type="text" name='name' placeholder='Name' required />
+                <br />
+                <input type="text" name='email' placeholder='Email' required />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default UpdateInventory;
